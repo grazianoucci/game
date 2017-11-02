632491bf21c32a8739c7178547a7b87ecffd8fbe
Stefano Fogarollo <sirfoga@protonmail.com>
2017-11-02 22:42:54 +0100

    merge main_algorithm_to_pool(...)

--- game.py
+++ game.py
@@ -281,7 +281,10 @@ def find_features_from_dict(to_predict):
     """
 
     output = []
-    keys = ["g0", "n", "NH", "U", "Z"]
+    if "AV" and "fesc" in to_predict:
+        keys = ["AV", "fesc"]
+    else:
+        keys = ["g0", "n", "NH", "U", "Z"]
 
     for k in keys:
         if k in to_predict in to_predict:
@@ -299,127 +302,7 @@ def main_algorithm_to_pool(
         labels_train, labels_test, labels, regr, line_labels,
         filename_int, filename_err, n_repetition, choice_rep, to_predict=None
 ):
-    g0, n, NH, U, Z, importances_g0, importances_n, importances_NH, \
-    importances_U, importances_Z = find_features_from_dict(to_predict)
-
-    mask = np.where(models == unique_id[i - 1])
-    matrix_mms = []  # matrix_mms is useful to save physical properties
-    index_find = []  # index_find helps to keep trace of the indexes
-    id_model = []
-
-    # Indexes for the labels:
-    # G/G0: 0, n: 1, NH: 2, U: 3, Z: 4
-    # Definition of training / testing
-    features_train = features[:, initial[mask][0]][:limit, :]
-    features_test = features[:, initial[mask][0]][limit:, :]
-
-    # ML error estimation
-    [g0_true, g0_pred, sigma_g0] = error_estimation(features_train,
-                                                    features_test,
-                                                    labels_train[:, 0],
-                                                    labels_test[:, 0], regr)
-    [n_true, n_pred, sigma_n] = error_estimation(features_train, features_test,
-                                                 labels_train[:, 1],
-                                                 labels_test[:, 1], regr)
-    [NH_true, NH_pred, sigma_NH] = error_estimation(features_train,
-                                                    features_test,
-                                                    labels_train[:, 2],
-                                                    labels_test[:, 2], regr)
-    [U_true, U_pred, sigma_U] = error_estimation(features_train, features_test,
-                                                 labels_train[:, 3],
-                                                 labels_test[:, 3], regr)
-    [Z_true, Z_pred, sigma_Z] = error_estimation(features_train, features_test,
-                                                 labels_train[:, 4],
-                                                 labels_test[:, 4], regr)
-
-    # Function calls for the machine learning routines
-    [model_g0, imp_g0, score_g0, std_g0] = machine_learning(
-        features[:, initial[mask][0]], labels, 0, regr)
-    [model_n, imp_n, score_n, std_n] = machine_learning(
-        features[:, initial[mask][0]], labels, 1, regr)
-    [model_NH, imp_NH, score_NH, std_NH] = machine_learning(
-        features[:, initial[mask][0]], labels, 2, regr)
-    [model_U, imp_U, score_U, std_U] = machine_learning(
-        features[:, initial[mask][0]], labels, 3, regr)
-    [model_Z, imp_Z, score_Z, std_Z] = machine_learning(
-        features[:, initial[mask][0]], labels, 4, regr)
-
-    # Bootstrap
-    new_data = realization(
-        filename_int, filename_err, n_repetition, mask
-    )[:, initial[mask][0]]
-
-    # Prediction of the physical properties
-    if choice_rep == YES:
-        for el in xrange(len(mask[0])):
-            g0[mask[0][el], :] = model_g0.predict(new_data[el::len(mask[0])])
-            n[mask[0][el], :] = model_n.predict(new_data[el::len(mask[0])])
-            NH[mask[0][el], :] = model_NH.predict(new_data[el::len(mask[0])])
-            U[mask[0][el], :] = model_U.predict(new_data[el::len(mask[0])])
-            Z[mask[0][el], :] = model_Z.predict(new_data[el::len(mask[0])])
-
-            # Model ids
-            id_model.append(i)
-            index_find.append(mask[0][el])
-            matrix_mms.append([g0[mask[0][el], :],
-                               n[mask[0][el], :],
-                               NH[mask[0][el], :],
-                               U[mask[0][el], :],
-                               Z[mask[0][el], :]])
-
-    if choice_rep == NO:
-        for el in xrange(len(mask[0])):
-            result = np.zeros((len(new_data[el::len(mask[0])]), 5))
-            result[:, 0] = model_g0.predict(new_data[el::len(mask[0])])
-            result[:, 1] = model_n.predict(new_data[el::len(mask[0])])
-            result[:, 2] = model_NH.predict(new_data[el::len(mask[0])])
-            result[:, 3] = model_U.predict(new_data[el::len(mask[0])])
-            result[:, 4] = model_Z.predict(new_data[el::len(mask[0])])
-
-            # Model ids
-            id_model.append(i)
-            index_find.append(mask[0][el])
-            vector_mms = np.zeros(15)
-            vector_mms[0::3] = np.log10(np.mean(10 ** result, axis=0))
-            vector_mms[1::3] = np.log10(np.median(10 ** result, axis=0))
-            vector_mms[2::3] = np.std(result, axis=0)
-            matrix_mms.append(vector_mms)
-
-    # Importance matrixes
-    importances_g0[initial[mask][0]] = imp_g0
-    importances_n[initial[mask][0]] = imp_n
-    importances_NH[initial[mask][0]] = imp_NH
-    importances_U[initial[mask][0]] = imp_U
-    importances_Z[initial[mask][0]] = imp_Z
-
-    # Print message
-    print 'Model', str(int(i)) + '/' + str(
-        int(np.max(unique_id))), 'completed...'
-
-    # Returns for the parallelization
-    return [sigma_g0, sigma_n, sigma_NH, sigma_U, sigma_Z], \
-           [i, score_g0, std_g0, score_n, std_n, score_NH, std_NH, score_U,
-            std_U, score_Z, std_Z], \
-           line_labels[initial[mask][0]], \
-           index_find, id_model, matrix_mms, \
-           [importances_g0, importances_n, importances_NH, importances_U,
-            importances_Z], \
-           [np.array(g0_true), np.array(n_true), np.array(NH_true),
-            np.array(U_true), np.array(Z_true)], \
-           [np.array(g0_pred), np.array(n_pred), np.array(NH_pred),
-            np.array(U_pred), np.array(Z_pred)]
-
-
-def main_algorithm_additional_to_pool(i
-                                      , models, unique_id, initial, limit
-                                      , features, labels_train, labels_test
-                                      , labels, regr, line_labels
-                                      , AV, fesc
-                                      , importances_AV, importances_fesc
-                                      , filename_int, filename_err,
-                                      n_repetition, choice_rep
-                                      ):
-    """ TODO all
+    """ TODO all docs
     :param i:
     :param models:
     :param unique_id:
@@ -431,109 +314,200 @@ def main_algorithm_additional_to_pool(i
     :param labels:
     :param regr:
     :param line_labels:
-    :param AV:
-    :param fesc:
-    :param importances_AV:
-    :param importances_fesc:
     :param filename_int:
     :param filename_err:
     :param n_repetition:
     :param choice_rep:
+    :param to_predict:
     :return:
     """
-    # TODO AP: I would recommend merging it with the main algorithm,
-    # by passing dictionary of features to be searched for, instead of AV,
-    # fesc (and n,... in the main)
 
-    mask = np.where(models == unique_id[i - 1])  # matrix_mms is useful to
-    # save physical properties
-    matrix_mms = []  # index_find helps to keep trace of the indexes
-    index_find = []
+    fesc_AV_mode = "AV" and "fesc" in to_predict
+
+    if fesc_AV_mode:
+        AV, fesc, importances_AV, importances_fesc = \
+            find_features_from_dict(to_predict)
+    else:
+        g0, n, NH, U, Z, importances_g0, importances_n, importances_NH, \
+        importances_U, importances_Z = find_features_from_dict(to_predict)
+
+    mask = np.where(models == unique_id[i - 1])
+    matrix_mms = []  # matrix_mms is useful to save physical properties
+    index_find = []  # index_find helps to keep trace of the indexes
     id_model = []
 
-    # Indexes for the additional labels:
-    # AV: 3, fesc: 4
+    # Indexes for the labels:
+    # G/G0: 0, n: 1, NH: 2, U: 3, Z: 4
+    # In case of AV AV: 3, fesc: 4
     # Definition of training / testing
     features_train = features[:, initial[mask][0]][:limit, :]
     features_test = features[:, initial[mask][0]][limit:, :]
 
     # ML error estimation
-    [AV_true, AV_pred, sigma_AV] = error_estimation(features_train,
-                                                    features_test,
-                                                    labels_train[:, 3],
-                                                    labels_test[:, 3], regr)
-    [fesc_true, fesc_pred, sigma_fesc] = error_estimation(features_train,
-                                                          features_test,
-                                                          labels_train[:, 4],
-                                                          labels_test[:, 4],
-                                                          regr)
+
+    if fesc_AV_mode:
+        [AV_true, AV_pred, sigma_AV] = error_estimation(
+            features_train,
+            features_test,
+            labels_train[:, 3],
+            labels_test[:, 3],
+            regr
+        )
+        [fesc_true, fesc_pred, sigma_fesc] = error_estimation(
+            features_train,
+            features_test,
+            labels_train[:, 4],
+            labels_test[:, 4],
+            regr
+        )
+    else:
+        [g0_true, g0_pred, sigma_g0] = error_estimation(
+            features_train,
+            features_test,
+            labels_train[:, 0],
+            labels_test[:, 0],
+            regr)
+        [n_true, n_pred, sigma_n] = error_estimation(
+            features_train, features_test,
+            labels_train[:, 1],
+            labels_test[:, 1], regr
+        )
+        [NH_true, NH_pred, sigma_NH] = error_estimation(
+            features_train,
+            features_test,
+            labels_train[:, 2],
+            labels_test[:, 2], regr
+        )
+        [U_true, U_pred, sigma_U] = error_estimation(
+            features_train, features_test,
+            labels_train[:, 3],
+            labels_test[:, 3], regr
+        )
+        [Z_true, Z_pred, sigma_Z] = error_estimation(
+            features_train, features_test,
+            labels_train[:, 4],
+            labels_test[:, 4], regr
+        )
+
     # Function calls for the machine learning routines
-    [model_AV, imp_AV, score_AV, std_AV] = machine_learning(
+
+    if fesc_AV_mode:
+        [model_AV, imp_AV, score_AV, std_AV] = machine_learning(
+            features[:, initial[mask][0]], labels, 3, regr)
+        [model_fesc, imp_fesc, score_fesc, std_fesc] = machine_learning(
+            features[:, initial[mask][0]], labels, 4, regr)
+    else:
+        [model_g0, imp_g0, score_g0, std_g0] = machine_learning(
+            features[:, initial[mask][0]], labels, 0, regr)
+        [model_n, imp_n, score_n, std_n] = machine_learning(
+            features[:, initial[mask][0]], labels, 1, regr)
+        [model_NH, imp_NH, score_NH, std_NH] = machine_learning(
+            features[:, initial[mask][0]], labels, 2, regr)
+        [model_U, imp_U, score_U, std_U] = machine_learning(
         features[:, initial[mask][0]], labels, 3, regr)
-    [model_fesc, imp_fesc, score_fesc, std_fesc] = machine_learning(
+        [model_Z, imp_Z, score_Z, std_Z] = machine_learning(
         features[:, initial[mask][0]], labels, 4, regr)
 
     # Bootstrap
-    new_data = realization(filename_int, filename_err, n_repetition, mask)[:,
-               initial[mask][0]]
+    new_data = realization(
+        filename_int, filename_err, n_repetition, mask
+    )[:, initial[mask][0]]
 
     # Prediction of the physical properties
     if choice_rep == YES:
         for el in xrange(len(mask[0])):
-            AV[mask[0][el], :] = model_AV.predict(new_data[el::len(mask[0])])
-            fesc[mask[0][el], :] = model_fesc.predict(
-                new_data[el::len(mask[0])])
+            if fesc_AV_mode:
+                AV[mask[0][el], :] = model_AV.predict(
+                    new_data[el::len(mask[0])])
+                fesc[mask[0][el], :] = model_fesc.predict(
+                    new_data[el::len(mask[0])])
+            else:
+                g0[mask[0][el], :] = model_g0.predict(
+                    new_data[el::len(mask[0])])
+                n[mask[0][el], :] = model_n.predict(new_data[el::len(mask[0])])
+                NH[mask[0][el], :] = model_NH.predict(
+                    new_data[el::len(mask[0])])
+                U[mask[0][el], :] = model_U.predict(new_data[el::len(mask[0])])
+                Z[mask[0][el], :] = model_Z.predict(new_data[el::len(mask[0])])
+
             # Model ids
             id_model.append(i)
             index_find.append(mask[0][el])
-            matrix_mms.append([AV[mask[0][el], :],
-                               fesc[mask[0][el], :]])
-    if choice_rep == NO:
+            matrix_mms.append([g0[mask[0][el], :],
+                               n[mask[0][el], :],
+                               NH[mask[0][el], :],
+                               U[mask[0][el], :],
+                               Z[mask[0][el], :]])
+    else:
         for el in xrange(len(mask[0])):
-            result = np.zeros((len(new_data[el::len(mask[0])]), 2))
-            result[:, 0] = model_AV.predict(new_data[el::len(mask[0])])
-            result[:, 1] = model_fesc.predict(new_data[el::len(mask[0])])
+            if fesc_AV_mode:
+                result = np.zeros((len(new_data[el::len(mask[0])]), 2))
+                result[:, 0] = model_AV.predict(new_data[el::len(mask[0])])
+                result[:, 1] = model_fesc.predict(new_data[el::len(mask[0])])
+            else:
+                result = np.zeros((len(new_data[el::len(mask[0])]), 5))
+                result[:, 0] = model_g0.predict(new_data[el::len(mask[0])])
+                result[:, 1] = model_n.predict(new_data[el::len(mask[0])])
+                result[:, 2] = model_NH.predict(new_data[el::len(mask[0])])
+                result[:, 3] = model_U.predict(new_data[el::len(mask[0])])
+                result[:, 4] = model_Z.predict(new_data[el::len(mask[0])])
+
             # Model ids
             id_model.append(i)
             index_find.append(mask[0][el])
-            #
-            vector_mms = np.zeros(6)
-            vector_mms[0::3] = np.mean(result, axis=0)
-            vector_mms[1::3] = np.median(result, axis=0)
+            vector_mms = np.zeros(15)
+            vector_mms[0::3] = np.log10(np.mean(10 ** result, axis=0))
+            vector_mms[1::3] = np.log10(np.median(10 ** result, axis=0))
             vector_mms[2::3] = np.std(result, axis=0)
             matrix_mms.append(vector_mms)
 
     # Importance matrixes
-    importances_AV[initial[mask][0]] = imp_AV
-    importances_fesc[initial[mask][0]] = imp_fesc
+    if fesc_AV_mode:
+        importances_AV[initial[mask][0]] = imp_AV
+        importances_fesc[initial[mask][0]] = imp_fesc
+    else:
+        importances_g0[initial[mask][0]] = imp_g0
+        importances_n[initial[mask][0]] = imp_n
+        importances_NH[initial[mask][0]] = imp_NH
+        importances_U[initial[mask][0]] = imp_U
+        importances_Z[initial[mask][0]] = imp_Z
 
     # Print message
     print 'Model', str(int(i)) + '/' + str(
         int(np.max(unique_id))), 'completed...'
 
     # Returns for the parallelization
-    return [sigma_AV, sigma_fesc], \
-           [i, score_AV, std_AV, score_fesc, std_fesc], \
-           line_labels[initial[mask][0]], \
-           index_find, id_model, matrix_mms, \
-           [importances_AV, importances_fesc], \
-           [np.array(AV_true), np.array(fesc_true)], \
-           [np.array(AV_pred), np.array(fesc_pred)]
+    if fesc_AV_mode:
+        return [sigma_AV, sigma_fesc], \
+               [i, score_AV, std_AV, score_fesc, std_fesc], \
+               line_labels[initial[mask][0]], \
+               index_find, id_model, matrix_mms, \
+               [importances_AV, importances_fesc], \
+               [np.array(AV_true), np.array(fesc_true)], \
+               [np.array(AV_pred), np.array(fesc_pred)]
+    else:
+        return [sigma_g0, sigma_n, sigma_NH, sigma_U, sigma_Z], \
+               [i, score_g0, std_g0, score_n, std_n, score_NH, std_NH, score_U,
+                std_U, score_Z, std_Z], \
+               line_labels[initial[mask][0]], \
+               index_find, id_model, matrix_mms, \
+               [importances_g0, importances_n, importances_NH, importances_U,
+                importances_Z], \
+               [np.array(g0_true), np.array(n_true), np.array(NH_true),
+                np.array(U_true), np.array(Z_true)], \
+               [np.array(g0_pred), np.array(n_pred), np.array(NH_pred),
+                np.array(U_pred), np.array(Z_pred)]
 
 
 def run_game(
-        manual_input=False
-        , filename_int='input/inputs_game_test.dat'
-        , filename_err='input/errors_game_test.dat'
-        , filename_library='input/labels_game_test.dat'
-        , choice_rep=YES
-        # AP: choice_rep should be asserted to be among the allowed values (
-        # even when input via raw_input).
-        , n_processes=2
-        , n_repetition=10000
-        , dir_path='output/'
-        , verbose=True
+        manual_input=False, filename_int='input/inputs_game_test.dat'
+        , filename_err='input/errors_game_test.dat',
+        filename_library='input/labels_game_test.dat', choice_rep=YES
+        , n_processes=2, n_repetition=10000, dir_path='output/', verbose=True
 ):
+    # AP: TODO choice_rep should be asserted to be among the allowed
+    # values even when input via raw_input).
+
     create_library_folder()
     if verbose:
         print INTRO
@@ -796,7 +770,7 @@ def run_game(
 
     # Searching for values of the additional physical properties
     pool = multiprocessing.Pool(processes=n_processes)
-    main_algorithm_additional = partial(main_algorithm_additional_to_pool,
+    main_algorithm_additional = partial(main_algorithm_to_pool,
                                         models=models, unique_id=unique_id,
                                         initial=initial, limit=limit
                                         , features=features,
@@ -838,6 +812,7 @@ def run_game(
     # find_ids are usefult to reorder the matrix with the ML determinations
     find_ids = list(chain.from_iterable(np.array(results)[:, 3]))
     temp_model_ids = list(chain.from_iterable(np.array(results)[:, 4]))
+
     if choice_rep == YES:
         temp_matrix_ml = np.array(
             list(chain.from_iterable(np.array(results)[:, 5])))
