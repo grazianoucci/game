d82f2225abad1a9657309dafcdc0d1935f956c94
Stefano Fogarollo <sirfoga@protonmail.com>
2017-11-02 21:50:17 +0100

    refactor main_algorithm_to_pool(...)

--- game.py
+++ game.py
@@ -14,6 +14,8 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.preprocessing import Normalizer
 
+YES, NO = "y", "n"
+
 
 def create_library_folder():
     """
@@ -216,17 +218,46 @@ def main_algorithm_to_pool(i
                            , filename_int, filename_err, n_repetition,
                            choice_rep
                            ):
+    """ TODO all
+    :param i:
+    :param models:
+    :param unique_id:
+    :param initial:
+    :param limit:
+    :param features:
+    :param labels_train:
+    :param labels_test:
+    :param labels:
+    :param regr:
+    :param line_labels:
+    :param g0:
+    :param n:
+    :param NH:
+    :param U:
+    :param Z:
+    :param importances_g0:
+    :param importances_n:
+    :param importances_NH:
+    :param importances_U:
+    :param importances_Z:
+    :param filename_int:
+    :param filename_err:
+    :param n_repetition:
+    :param choice_rep:
+    :return:
+    """
+
     mask = np.where(models == unique_id[i - 1])
-    # matrix_mms is useful to save physical properties
-    matrix_mms = []
-    # index_find helps to keep trace of the indexes
-    index_find = []
+    matrix_mms = []  # matrix_mms is useful to save physical properties
+    index_find = []  # index_find helps to keep trace of the indexes
     id_model = []
+
     # Indexes for the labels:
     # G/G0: 0, n: 1, NH: 2, U: 3, Z: 4
     # Definition of training / testing
     features_train = features[:, initial[mask][0]][:limit, :]
     features_test = features[:, initial[mask][0]][limit:, :]
+
     # ML error estimation
     [g0_true, g0_pred, sigma_g0] = error_estimation(features_train,
                                                     features_test,
@@ -245,6 +276,7 @@ def main_algorithm_to_pool(i
     [Z_true, Z_pred, sigma_Z] = error_estimation(features_train, features_test,
                                                  labels_train[:, 4],
                                                  labels_test[:, 4], regr)
+
     # Function calls for the machine learning routines
     [model_g0, imp_g0, score_g0, std_g0] = machine_learning(
         features[:, initial[mask][0]], labels, 0, regr)
@@ -256,17 +288,21 @@ def main_algorithm_to_pool(i
         features[:, initial[mask][0]], labels, 3, regr)
     [model_Z, imp_Z, score_Z, std_Z] = machine_learning(
         features[:, initial[mask][0]], labels, 4, regr)
+
     # Bootstrap
-    new_data = realization(filename_int, filename_err, n_repetition, mask)[:,
-               initial[mask][0]]
+    new_data = realization(
+        filename_int, filename_err, n_repetition, mask
+    )[:, initial[mask][0]]
+
     # Prediction of the physical properties
-    if choice_rep == 'y':
+    if choice_rep == YES:
         for el in xrange(len(mask[0])):
             g0[mask[0][el], :] = model_g0.predict(new_data[el::len(mask[0])])
             n[mask[0][el], :] = model_n.predict(new_data[el::len(mask[0])])
             NH[mask[0][el], :] = model_NH.predict(new_data[el::len(mask[0])])
             U[mask[0][el], :] = model_U.predict(new_data[el::len(mask[0])])
             Z[mask[0][el], :] = model_Z.predict(new_data[el::len(mask[0])])
+
             # Model ids
             id_model.append(i)
             index_find.append(mask[0][el])
@@ -275,7 +311,7 @@ def main_algorithm_to_pool(i
                                NH[mask[0][el], :],
                                U[mask[0][el], :],
                                Z[mask[0][el], :]])
-    if choice_rep == 'n':
+    if choice_rep == NO:
         for el in xrange(len(mask[0])):
             result = np.zeros((len(new_data[el::len(mask[0])]), 5))
             result[:, 0] = model_g0.predict(new_data[el::len(mask[0])])
@@ -283,24 +319,27 @@ def main_algorithm_to_pool(i
             result[:, 2] = model_NH.predict(new_data[el::len(mask[0])])
             result[:, 3] = model_U.predict(new_data[el::len(mask[0])])
             result[:, 4] = model_Z.predict(new_data[el::len(mask[0])])
+
             # Model ids
             id_model.append(i)
             index_find.append(mask[0][el])
-            #
             vector_mms = np.zeros(15)
             vector_mms[0::3] = np.log10(np.mean(10 ** result, axis=0))
             vector_mms[1::3] = np.log10(np.median(10 ** result, axis=0))
             vector_mms[2::3] = np.std(result, axis=0)
             matrix_mms.append(vector_mms)
+
     # Importance matrixes
     importances_g0[initial[mask][0]] = imp_g0
     importances_n[initial[mask][0]] = imp_n
     importances_NH[initial[mask][0]] = imp_NH
     importances_U[initial[mask][0]] = imp_U
     importances_Z[initial[mask][0]] = imp_Z
+
     # Print message
     print 'Model', str(int(i)) + '/' + str(
         int(np.max(unique_id))), 'completed...'
+
     # Returns for the parallelization
     return [sigma_g0, sigma_n, sigma_NH, sigma_U, sigma_Z], \
            [i, score_g0, std_g0, score_n, std_n, score_NH, std_NH, score_U,
